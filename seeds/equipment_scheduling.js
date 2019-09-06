7e07125c8c192ac36607d2feacd8c547da516cbb
dev-sedepti <teoian.giovane@gmail.com>
2019-09-06 13:39:33 -0300

    Cria seed de equipamentos_agendamentos

---
+++ seeds/equipment_scheduling.js
@@ -0,0 +1,17 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('equipment_scheduling')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('equipment_scheduling').insert([
+        { id: 1, equipment_id: 2, scheduling_id: 1 },
+        { id: 2, equipment_id: 3, scheduling_id: 1 },
+        { id: 3, equipment_id: 1, scheduling_id: 2 },
+        { id: 4, equipment_id: 1, scheduling_id: 3 },
+        { id: 5, equipment_id: 1, scheduling_id: 4 },
+        { id: 6, equipment_id: 2, scheduling_id: 4 },
+        { id: 7, equipment_id: 3, scheduling_id: 4 }
+      ])
+    })
+}
