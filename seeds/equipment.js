a2d6c761c5e3c25ac572be369da93c6f316daafa
dev-sedepti <teoian.giovane@gmail.com>
2019-09-06 13:41:56 -0300

    Corrige nome da seed de equipamentos

--- seeds/equipment.js
+++ seeds/equipment.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name')
+  return knex('equipment')
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('equipment').insert([
         { id: 1, name: 'Notebook' },
         { id: 2, name: 'Datashow' },
         { id: 3, name: 'Quadro interativo' }
