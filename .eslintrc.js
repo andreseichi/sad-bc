6459184de4b7262ca31ea843d076c15a10ddc95f
quaresmateo <teoian.giovane@gmail.com>
2019-11-01 13:37:59 -0300

    Corrige eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
-  // required to lint *.vue files
+  globals: {
+    use: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
   plugins: [
-    'html'
+    'prettier'
   ],
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // do not allow console.logs etc...
-    'no-console': 2
-  },
-  globals: {
-    'use': true
+    'nuxt/no-cjs-in-config': 'off'
   }
 }
