b840748ae212bcfb2cea22cb05b0d147ecbecd2d
quaresmateo <teoian.giovane@gmail.com>
2019-11-19 13:22:07 -0300

    Refatora e elimina 'member' do campo role da tabela de Users

--- database/migrations/1572623002210_users_schema.js
+++ database/migrations/1572623002210_users_schema.js
@@ -7,17 +7,19 @@ class UsersSchema extends Schema {
   up() {
     this.create('users', (table) => {
       table.increments()
-      table.string('email').notNullable().unique()
+      table
+        .string('email')
+        .notNullable()
+        .unique()
       table.string('name').notNullable()
       table.string('surname')
       table.date('birthday', { precision: 6 })
       table.string('password').notNullable()
       table.enum('gender', ['female', 'male', 'other'])
-      table.enum('role', [
-        'admin',
-        'member',
-        'event-manager'
-      ]).notNullable().defaultTo('event-manager')
+      table
+        .enum('role', ['admin', 'event-manager'])
+        .notNullable()
+        .defaultTo('event-manager')
       table.timestamps()
     })
   }
